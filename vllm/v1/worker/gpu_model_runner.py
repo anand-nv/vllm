ec60f51a367c5deeb3ce0353349003a3906d5e96
Andrew Liu <landrew@nvidia.com>
2025-10-11 20:56:38 -0700

    [WIP] Amend hidden state return logic
    Some minor changes affecting correctness of the hidden state return logic were
    lost when copying to the new branch.

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -2717,9 +2717,9 @@ class GPUModelRunner(LoRAModelRunnerMixin, KVConnectorModelRunnerMixin):
         hidden_states_list = []
         for i in range(num_reqs):
             start = int(query_start_loc_np[i])
-            end = int(num_scheduled_tokens_np[i])
+            length = int(num_scheduled_tokens_np[i])
             hidden_states_list.append(
-                hidden_states[start:end].cpu()
+                hidden_states[start:start+length].cpu()
             )
 
         output = ModelRunnerOutput(

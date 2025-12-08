ab880fb13555ad81ce87d032bc784fda508bf7ff
Viacheslav Klimkov <vklimkov@nvidia.com>
2025-12-08 17:46:58 +0100

    rebase fastconformer on top of alm_api_v3, preparing to implement streaming version of preprocessor
    Signed-off-by: Viacheslav Klimkov <vklimkov@nvidia.com>

--- vllm/model_executor/models/registry.py
+++ vllm/model_executor/models/registry.py
@@ -164,6 +164,7 @@ _TEXT_GENERATION_MODELS = {
     "SolarForCausalLM": ("solar", "SolarForCausalLM"),
     "TeleChat2ForCausalLM": ("telechat2", "TeleChat2ForCausalLM"),
     "TeleFLMForCausalLM": ("teleflm", "TeleFLMForCausalLM"),
+    "ToyConv": ("toy_conv", "ToyConv"),
     "XverseForCausalLM": ("llama", "LlamaForCausalLM"),
     "Zamba2ForCausalLM": ("zamba2", "Zamba2ForCausalLM"),
 }

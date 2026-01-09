2bc14db361537a65931c49186959bb47f202d55a
Viacheslav Klimkov <vklimkov@nvidia.com>
2026-01-09 20:48:02 +0100

    Trying to fix new kernels to work both in prefill and decoding

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -68,7 +68,7 @@ from vllm.model_executor.model_loader.reload import (
 )
 from vllm.model_executor.models.deepseek_v2 import DeepseekV32IndexerCache
 from vllm.model_executor.models.fastconformer import ConformerConvModule
-from vllm.model_executor.models.toy_conv import ToyConv2dLayer
+from vllm.model_executor.models.toy_conv import ToyConv2dLayer, MelSpectrogramLayer
 from vllm.model_executor.models.interfaces import (
     MultiModalEmbeddings,
     SupportsMRoPE,
@@ -6948,6 +6948,11 @@ class GPUModelRunner(
         )
         for layer_name, toy_conv_module in toy_conv_layers.items():
             kv_cache_spec[layer_name] = toy_conv_module.get_kv_cache_spec()
+        stft_layers = get_layers_from_vllm_config(
+            self.vllm_config, MelSpectrogramLayer
+        )
+        for layer_name, stft_module in stft_layers.items():
+            kv_cache_spec[layer_name] = stft_module.get_kv_cache_spec()
 
         return kv_cache_spec
 
